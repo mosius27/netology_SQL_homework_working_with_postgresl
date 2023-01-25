cc2d2b3d9121ae0008944a99c08478998dcacf85
mosius27 <mosius27@yandex.ru>
2023-01-25 21:10:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,145 +7,133 @@ class Working_with_postgresql():
 
         with open('db_access.yml', 'r', encoding='utf-8') as file: 
             self.settings = yaml.load(file, Loader=yaml.FullLoader)
+                
 
     def create_db_structure(self):
 
         try:
-            conn = self.db_connect()
-
-            with conn.cursor() as cursor:
-                cursor.execute("""
-                            CREATE TABLE IF NOT EXISTS clients (
-                                id SERIAL PRIMARY KEY,
-                                first_name VARCHAR(50) NOT NULL,
-                                last_name VARCHAR(50) NOT NULL,
-                                email VARCHAR(255) NOT NULL
-                            );""")
-                cursor.execute("""
-                                CREATE TABLE IF NOT EXISTS phones (
+            with self.db_connect() as conn:
+                with conn.cursor() as cursor:
+                    cursor.execute("""
+                                CREATE TABLE IF NOT EXISTS clients (
                                     id SERIAL PRIMARY KEY,
-                                    phone VARCHAR(20) NOT NULL,
-                                    client_id INTEGER NOT NULL,
-                                    FOREIGN KEY (client_id) REFERENCES clients (id)
+                                    first_name VARCHAR(50) NOT NULL,
+                                    last_name VARCHAR(50) NOT NULL,
+                                    email VARCHAR(255) NOT NULL
                                 );""")
-
-            conn.commit()
+                    cursor.execute("""
+                                    CREATE TABLE IF NOT EXISTS phones (
+                                        id SERIAL PRIMARY KEY,
+                                        phone VARCHAR(20) NOT NULL,
+                                        client_id INTEGER NOT NULL,
+                                        FOREIGN KEY (client_id) REFERENCES clients (id)
+                                    );""")
 
         except Exception as _ex:
             print("Error while working with PostgreSQL", _ex)
-        finally:
-            if conn:
-                conn.close()
-                print("PostgreSQL connection closed")
+        
 
     def add_client(self, first_name, last_name, email):
 
         try:
-            conn = self.db_connect()
-            
-            with conn.cursor() as cursor:
-                cursor.execute(f"INSERT INTO clients (first_name, last_name, email) VALUES ('{first_name}', '{last_name}', '{email}');")
-
-            conn.commit()
+            with self.db_connect() as conn:
+                with conn.cursor() as cursor:
+                    cursor.execute("INSERT INTO clients (first_name, last_name, email) VALUES (%s, %s, %s);", 
+                                (first_name, last_name, email))
         
         except Exception as _ex:
             print("Error while working with PostgreSQL", _ex)
-        finally:
-            if conn:
-                conn.close()
-                print("PostgreSQL connection closed")
+        
 
     def add_phone(self, client_id, phone):
         
         try:
-            conn = self.db_connect()
-            
-            with conn.cursor() as cursor:
-                cursor.execute(f"INSERT INTO phones (client_id, phone) VALUES ({client_id}, '{phone}');")
-            
-            conn.commit()
+
+            with self.db_connect() as conn:
+                with conn.cursor() as cursor:
+                    cursor.execute("INSERT INTO phones (client_id, phone) VALUES (%s, %s);",
+                                    (client_id, phone))
         
         except Exception as _ex:
             print("Error while working with PostgreSQL", _ex)
-        finally:
-            if conn:
-                conn.close()
-                print("PostgreSQL connection closed")
+        
 
     def update_client(self, id, first_name, last_name, email):
 
         try:
-            conn = self.db_connect()
 
-            with conn.cursor() as cursor:
-                cursor.execute(f"UPDATE clients SET first_name = '{first_name}', last_name = '{last_name}',\
-                                email = '{email}' WHERE id = {id};")
-
-            conn.commit()
+            with self.db_connect() as conn:
+                with conn.cursor() as cursor:
+                    query = "UPDATE clients SET "
+                    params = []
+                    if first_name:
+                        query += "first_name = %s,"
+                        params.append(first_name)
+                    if last_name:
+                        query += "last_name = %s,"
+                        params.append(last_name)
+                    if email:
+                        query += "email = %s,"
+                        params.append(email)
+                    query = query[:-1] + " WHERE id = %s"
+                    params.append(id)
+                    cursor.execute(query, params)
         
         except Exception as _ex:
             print("Error while working with PostgreSQL", _ex)
-        finally:
-            if conn:
-                conn.close()
-                print("PostgreSQL connection closed")
 
     def delete_phone(self, client_id, phone):
         
         try:
-            conn = self.db_connect()
-            
-            with conn.cursor() as cursor:
-                cursor.execute(f"DELETE FROM phones WHERE client_id = {client_id} AND phone = '{phone}';")
-            
-            conn.commit()
+            with self.db_connect() as conn:
+                with conn.cursor() as cursor:
+                    cursor.execute("DELETE FROM phones WHERE client_id = %s AND phone = '%s';", (client_id, phone))
         
         except Exception as _ex:
             print("Error while working with PostgreSQL", _ex)
-        finally:
-            if conn:
-                conn.close()
-                print("PostgreSQL connection closed")
+        
 
     def delete_client(self, client_id):
         
         try:
-            conn = self.db_connect()
-
-            with conn.cursor() as cursor:
-                cursor.execute(f'DELETE FROM phones WHERE client_id = {client_id}')
-                cursor.execute(f"DELETE FROM clients WHERE id = {client_id};")
-            
-            conn.commit()
+            with self.db_connect() as conn:
+                with conn.cursor() as cursor:
+                    cursor.execute('DELETE FROM phones WHERE client_id = %s;', (client_id,))
+                    cursor.execute("DELETE FROM clients WHERE id = %s;", (client_id,))
         
         except Exception as _ex:
             print("Error while working with PostgreSQL", _ex)
-        finally:
-            if conn:
-                conn.close()
-                print("PostgreSQL connection closed")
+        
 
-    def find_client(self, query):
+    def find_client(self, first_name, last_name, email, phone):
         try:
-            conn = self.db_connect()
+            with self.db_connect() as conn:
+                with conn.cursor() as cursor:
+                    conditions = []
+                    if first_name:
+                        conditions.append(f"first_name = '{first_name}'")
+                    if last_name:
+                        conditions.append(f"last_name = '{last_name}'")
+                    if email:
+                        conditions.append(f"email = '{email}'")
+                    if phone:
+                        conditions.append(f"phone = '{phone}'")
 
-            with conn.cursor() as cursor:
-                cursor.execute(f"SELECT * FROM clients \
-                        LEFT JOIN phones ON clients.id = phones.client_id\
-                        WHERE first_name ILIKE '{query}' \
-                        OR last_name ILIKE '{query}' \
-                        OR email ILIKE '{query}' \
-                        OR phone ILIKE '{query}';")
-                result = cursor.fetchall()
-        
+                    # cursor.execute(f"SELECT * FROM clients \
+                    #         LEFT JOIN phones ON clients.id = phones.client_id\
+                    #         WHERE first_name ILIKE '{query}' \
+                    #         OR last_name ILIKE '{query}' \
+                    #         OR email ILIKE '{query}' \
+                    #         OR phone ILIKE '{query}';")
+
+                    query = "SELECT * FROM clients LEFT JOIN phones ON clients.id = phones.client_id WHERE " + " AND ".join(conditions)
+                    cursor.execute(query)
+                    result = cursor.fetchall()
+            
+            return result
+            
         except Exception as _ex:
             print("Error while working with PostgreSQL", _ex)
-        finally:
-            if conn:
-                conn.close()
-                print("PostgreSQL connection closed")
-
-        return result
 
     def db_connect(self):
         connect = pg.connect(
@@ -188,9 +176,9 @@ if __name__ == "__main__":
 
         elif action == '4':
             client_id = input('Введите id клиента: ')
-            first_name = input('Введите имя клиента: ')
-            last_name = input('Введите фамилию пользователя: ')
-            email = input('Введите email клиента: ')
+            first_name = input('Введите имя клиента(чтобы оставить прежнее значение оставьте пустым): ').strip()
+            last_name = input('Введите фамилию пользователя(чтобы оставить прежнее значение оставьте пустым): ').strip()
+            email = input('Введите email клиента(чтобы оставить прежнее значение оставьте пустым): ').strip()
             work_with_db.update_client(client_id, first_name, last_name, email)
 
         elif action == '5':
@@ -203,8 +191,12 @@ if __name__ == "__main__":
             work_with_db.delete_client(client_id)
 
         elif action == '7':
-            query = input('Введите имя/фамилию/email/номер телефона клиента: ')
-            result = work_with_db.find_client(query)
+            print('Укажите данные по которым осуществлять поиск')
+            first_name = input('Имя клиента(оставить пустым чтобы не производить поиск по данному параметру): ')
+            last_name = input('Фамилия клиента(оставить пустым чтобы не производить поиск по данному параметру): ')
+            email = input('Email(оставить пустым чтобы не производить поиск по данному параметру): ')
+            phone = input('Номер телефона(оставить пустым чтобы не производить поиск по данному параметру): ')
+            result = work_with_db.find_client(first_name, last_name, email, phone)
             print(result)
 
         elif action == '0':
